8fc68ac246a39039199cea130e15f3fdf24b35c1
morningLemon <ian.summs@gmail.com>
2017-01-17 14:35:56 +0200

    Tutorial vid5 - set up mail service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import { MailService } from './mail.service';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
         <app-simple-form></app-simple-form>
+
+        {{ mail.message }}
     </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Server running :)';
+
+  constructor(private mail: MailService) {}
 }
